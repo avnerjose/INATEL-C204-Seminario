ba104d425b41c72093ee5e3f3500b86ba9a4aa1b
antoniov-01 <antonio.v@ges.inatel.br>
2022-10-07 21:29:02 -0300

    UseCaseDelete

--- src/useCases/DeleteGameController/index.js
+++ src/useCases/DeleteGameController/index.js
@@ -1,7 +1,9 @@
 const { GamesRepository } = require("../../repositories/GamesRepository");
 const { DeleteGameController } = require("./DeleteGameController");
+const { DeleteGameUseCase } = require("./DeleteGameUseCase");
 
 const gamesRepository = GamesRepository.getInstance();
+const DeleteGameUseCase = new DeleteGameUseCase(gamesRepository);
 const deleteGameController = new DeleteGameController(gamesRepository);
 
 module.exports = { deleteGameController };
