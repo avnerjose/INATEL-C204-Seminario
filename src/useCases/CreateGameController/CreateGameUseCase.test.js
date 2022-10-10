297eaf8f992f75d57164a7c1e45f5b322cdf61eb
avnerjose <avnerj.g.r@gmail.com>
2022-10-10 18:05:43 -0300

    Removendo comentários do teste

--- src/useCases/CreateGameController/CreateGameUseCase.test.js
+++ src/useCases/CreateGameController/CreateGameUseCase.test.js
@@ -1,6 +1,7 @@
 const { Game } = require("../../models/Game");
 const { GamesRepository } = require("../../repositories/GamesRepository");
 const { CreateGameUseCase } = require("./CreateGameUseCase");
+
 //O describe serve para unir testes de um mesma classe em um único contexto
 describe("CreateGameUseCase", () => {
   let gamesRepository;
@@ -15,28 +16,26 @@ describe("CreateGameUseCase", () => {
   };
 
   beforeEach(() => {
-    //Antes de cada teste instancia os objetos e aplica a injeção de dependência
     gamesRepository = new GamesRepository();
     createGameUseCase = new CreateGameUseCase(gamesRepository);
   });
 
-  //Cada teste unitário é um "it" ou um "test"
   it("should be able to create a game", () => {
-    const resultGame = createGameUseCase.execute(mockedGame); //Execute a criação do game
+    const resultGame = createGameUseCase.execute(mockedGame);
 
-    expect(gamesRepository.list()).toHaveLength(1); //A lista de games deveria ter 1 de tamanho
-    expect(gamesRepository.list()[0]).toBeInstanceOf(Game); //O game criado deve ser uma instância da classe Game
+    expect(gamesRepository.list()).toHaveLength(1);
+    expect(gamesRepository.list()[0]).toBeInstanceOf(Game);
     expect(gamesRepository.list()[0]).toEqual(
       expect.objectContaining(mockedGame)
-    ); //O game criado deve ter os dados mockados passados na criação
+    );
     expect(resultGame).toEqual(expect.objectContaining(mockedGame));
   });
 
   it("should not be able to create a test with a name already taken", () => {
-    createGameUseCase.execute(mockedGame); //Cria um game
+    createGameUseCase.execute(mockedGame);
 
     expect(() => {
-      createGameUseCase.execute(mockedGame); //Tenta criar um outro game com o mesmo nome
-    }).toThrow(); //Espera que uma exceção seja lançada
+      createGameUseCase.execute(mockedGame);
+    }).toThrow();
   });
 });
